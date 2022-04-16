108fa262bfbee340c16b2e00667e444af0d6b5e7
Josh Huang <josh_huang@joshs-mbp.myfiosgateway.com>
2022-04-16 04:44:42 -0400

    finished a9

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ import helloController
     from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
-var server_port = process.env.PORT || 4000;
-var server_host = process.env.HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+const server_port = process.env.PORT || 4000;
+const server_host = process.env.HOST || '0.0.0.0';
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://127.0.0.1/webdev'
 mongoose.connect(CONNECTION_STRING);
@@ -31,6 +29,6 @@ helloController(app);
 userController(app);
 tuitsController(app);
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
